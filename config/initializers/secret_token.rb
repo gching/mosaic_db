940b7b194b4ea0135c0bcbab1a3af7bff1340ff1
Gavin Ching <gavinchingy@gmail.com>
2013-05-22 15:50:14 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MosaicDb::Application.config.secret_token = '02f97130d524173bef5520a3209def7f3f0f3bcbe09147ef4e7154336fd12bee9a9ca3d903f88837e3679f204ebf1db2ed22f5f182f4b82c7c901644b273208a'
